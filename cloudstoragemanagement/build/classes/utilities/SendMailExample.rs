0d97779f75cee3f7ac1fb351596dafec97633c9f
j-anurag <anurag.j.30122003@gmail.com>
2025-06-06 15:10:18 +0530

    first commit

---
+++ cloudstoragemanagement/build/classes/utilities/SendMailExample.rs
@@ -0,0 +1,2 @@
+utilities.SendMailExample
+utilities.SMTPAuthenticator
---
+++ cloudstoragemanagement/src/utilities/ips.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ips 
+{
+//    public static String cmsip="localhost";
+//    public static String storage1ip="localhost";
+//    public static String storage2ip="localhost";
+//    public static String storage3ip="localhost";
+    public static String cmsip="192.168.127.123";
+    public static String storage1ip="192.168.127.23";
+    public static String storage2ip="192.168.127.23";
+    public static String storage3ip="192.168.127.23";
+    
+}
